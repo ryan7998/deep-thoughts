b4f57da99646e1dcae65a4a36181693465bab846
fazle <ryan7998@gmail.com>
2021-04-27 13:57:20 -0400

    reaction added

--- client/src/components/ReactionForm/index.js
+++ client/src/components/ReactionForm/index.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/react-hooks';
+import { ADD_REACTION } from '../../utils/mutations';
 
 const ReactionForm = ({ thoughtId }) => {
     const [reactionBody, setBody] = useState('');
@@ -13,14 +15,29 @@ const ReactionForm = ({ thoughtId }) => {
 
     const handleFormSubmit = async event => {
         event.preventDefault();
-        setBody('');
-        setCharacterCount(0);
+        try {
+            // add thought to database
+            await addReaction({
+              variables: { reactionBody, thoughtId }
+            });
+      
+            // clear form value
+            setBody('');
+            setCharacterCount(0);
+          } catch (e) {
+            console.error(e);
+          }
+
+
     };
+
+    const [addReaction, { error }] = useMutation(ADD_REACTION);
 
     return (
         <div>
-            <p className={`m-0`}>
+            <p className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}>
                 Character Count: {characterCount}/280
+                {error && <span className="ml-2">Something went wrong...</span>}
             </p>
             <form 
                 className="flex-row justify-center justify-space-between-md align-stretch"
