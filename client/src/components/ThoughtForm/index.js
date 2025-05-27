6453ec6634b8fa3d5e21e7aca59e61aee424733a
Fazle Ryan Chowdhury <ryan7998@gmail.com>
2025-05-26 22:52:03 -0400

    Update index.js

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,8 +1,4 @@
 import React, { useState } from 'react';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-
-
 import { useMutation } from '@apollo/client';
 import { ADD_THOUGHT } from '../../utils/mutations';
 import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
